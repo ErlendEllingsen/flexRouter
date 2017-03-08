eed0a83f125122c880e93c90ebd9ba24f9cd0e14
Robert Cox <rcox@dontbebroke.com>
2017-03-08 09:46:28 -0800

    Configured and setup phpunit

---
+++ tests/FlexRouter/FlexRouterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FlexRouter\Tests;
+
+use FlexRouter\FlexRouter;
+use PHPUnit\Framework\TestCase;
+
+class FlexRouterTest extends TestCase
+{
+    /**
+     * The initial test to build on
+     */
+    public function testNonWildCardGET()
+    {
+        $router = new FlexRouter();
+        $route = $router->route('GET', '/');
+
+        $this->assertEquals($route, true);
+    }
+}
